87a76677bde676345cc6cb25f6ff95a2943ed5ad
h8_s <h8_s@usa.com>
2021-03-11 22:29:37 +0100

    Remove all colors from ipython

---
+++ ipython/profile_default/ipython_config.py
@@ -0,0 +1,5 @@
+# use colors for displaying information about objects
+c.InteractiveShell.color_info = False
+
+# color scheme (NoColor, Neutral, Linux, or LightBG).
+c.InteractiveShell.colors = 'NoColor'
