71e5c816fc0a0aa6562e51a008eb8b9753a12db6
h8_s <h8_s@usa.com>
2021-09-20 19:30:10 +0200

    Remove ipython config

--- ipython/profile_default/ipython_config.py
+++
@@ -1,5 +0,0 @@
-# use colors for displaying information about objects
-c.InteractiveShell.color_info = False
-
-# color scheme (NoColor, Neutral, Linux, or LightBG).
-c.InteractiveShell.colors = 'NoColor'
